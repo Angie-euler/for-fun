cd8fa880e86fccd77f423544a7fc9897ab3088de
Angie-euler <ali1434346859@gmail.com>
2020-07-03 15:05:21 +0800

    unittest.py

---
+++ unittest.py
@@ -0,0 +1,26 @@
+import json
+import unittest
+#from demo import AnonymousSurvey
+"""question="What langue did you first learn to speak?"
+my_survey=AnonymousSurvey(question)
+my_survey.show_question()
+print("Enter 'q' at any time quite.\n")
+while True:
+    response=input("Langue:")
+    if response=='q':
+        break
+    my_survey.store_responses(response)
+    print("\nThank you to everyone who particpated in the survey!")
+    my_survey.show_results()
+"""
+"""class TestAnonmyousSurvey(unittest.TestCase):
+    def test_store_sigle_esponse(self):
+        question="What langue did you first learn to speak?"
+        my_survey=AnonymousSurvey(question)
+        my_survey.store_responses("Chinese")
+        self.assertIn('Chinese',my_survey.responses)
+unittest.main()
+"""
+
+
+
